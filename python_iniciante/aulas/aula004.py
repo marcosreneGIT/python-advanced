08cba1dd912c5c843b07c8bcf5ca342b485a49d4
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-02-27 19:51:32 -0300

    Create aula004.py

---
+++ python_iniciante/aulas/aula004.py
@@ -0,0 +1,23 @@
+# tipos int e float
+
+# int - número inteiro
+# o tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado positivo.
+
+print(11) # int
+print(-1) # int
+print(00) # int
+
+# float - número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+
+print(1.1, 10.11) # float
+print(0.0, -10.5) # float
+
+# a função type mostra o tipo que o Python inferiu ao valor.
+
+print(type('Renê'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
