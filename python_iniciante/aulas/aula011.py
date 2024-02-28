089ba6b7985224fae896338f83954f06234e6ff0
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-02-28 18:29:48 -0300

    python

---
+++ python_iniciante/aulas/aula011.py
@@ -0,0 +1,8 @@
+nome = 'Marcos Renê'
+peso = 100
+altura = 1.93
+
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc)
+print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc}')
---
+++ python_iniciante/exercicios/ex002.py
@@ -0,0 +1,7 @@
+nome = 'Marcos Renê'
+peso = 100
+altura = 1.93
+
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e seu IMC é', imc)
