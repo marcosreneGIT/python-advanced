c3d770c930cb39624b618ecd27b28c0fcc81982f
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-02-29 16:41:33 -0300

    Create aula020.py

---
+++ python_iniciante/aulas/aula020.py
@@ -0,0 +1,14 @@
+# operadores in e not in
+
+# strings são iteráveis
+#  0 1 2 3 4 5
+#  O t á v i o
+# -6-5-4-3-2-1
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
