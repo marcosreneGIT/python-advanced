29095475db240c5fc195817f7b9172c67f1fdb83
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-03-11 17:45:24 -0300

    Create aula028.py

---
+++ python_iniciante/aulas/aula028.py
@@ -0,0 +1,11 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'marcos Renê'
+print(string.capitalize())
+
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(outra_variavel)
+
