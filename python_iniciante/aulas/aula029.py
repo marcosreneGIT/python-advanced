8ad1088553ed0a2ea0e66a5cebcead5cfcb33161
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-03-12 19:25:55 -0300

    Create aula029.py

---
+++ python_iniciante/aulas/aula029.py
@@ -0,0 +1,15 @@
+"""
+repetição: while (enquanto)
+executa uma ação enquanto uma condição for verdadeira
+loop infinito - quando um código não tem fim
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
