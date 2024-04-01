3046a9363d6358bccdcb74b31ab3798977ffe623
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-04-01 20:31:50 -0300

    Create aula053.py

---
+++ python_iniciante/aulas/aula053.py
@@ -0,0 +1,18 @@
+# Desempacotamento em chamadas de métodos e funções
+
+string = 'ABCD'
+
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+
+tupla = 'Python', 'é', 'legal'
+
+p, b, *_, ap, u = lista
+print(p, u, ap)
+
+print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+print(*lista)
+
+# print(*string)
+# print(*tupla)
+
+print(*lista, sep='\n')
