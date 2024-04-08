78c82fae54b1d2d29940e9784a3f05eba994540d
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-04-08 20:14:16 -0300

    Create aula057.py

---
+++ python_intermedirario/aulas/aula057.py
@@ -0,0 +1,22 @@
+"""
+valores padrão para parâmetros
+
+ao definir uma função, os parâmetros podem ter valores padrão. 
+caso o valor não seja enviado para o parâmetro, o valor padrão será usado.
+
+refatorar: editar o seu código.
+"""
+
+
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+
+soma(1, 2)
+soma(3, 5)
+soma(100, 200)
+soma(7, 9, 0)
+soma(y=9, z=0, x=7)
