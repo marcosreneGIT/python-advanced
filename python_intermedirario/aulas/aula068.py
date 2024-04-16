fc51f55ceb007677553bdd50c74430af3c7b61de
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-04-16 17:41:58 -0300

    Create aula068.py

---
+++ python_intermedirario/aulas/aula068.py
@@ -0,0 +1,15 @@
+# Sets - Conjuntos em Python (tipo set)
+
+# Conjuntos são ensinados na matemática
+# Representados graficamente pelo diagrama de Venn
+
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+
+set_0 = set()  # vazio
+set_1 = {'Marcos', 1, 2, 3}  # com dados
+
+
