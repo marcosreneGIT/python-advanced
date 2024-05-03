d6b6a99350d550c852053d232475e983bdb4cc63
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-05-03 16:34:20 -0300

    aula077.py

--- python_intermedirario/aulas/aula077.py
+++ python_intermedirario/aulas/aula077.py
@@ -1,7 +1,8 @@
 produtos = [
-    {'nome': 'feijão', 'preco': 10, },
-    {'nome': 'arroz', 'preco': 5,   },
-    {'nome': 'café', 'preco': 8,    },
+    {'nome': 'feijão', 'preco': 10, 'tipo': 'alimento', },
+    {'nome': 'arroz' , 'preco': 5 , 'tipo': 'alimento', },
+    {'nome': 'café'  , 'preco': 8 , 'tipo': 'alimento', },
+    {'nome': 'sabão' , 'preco': 3 , 'tipo': 'higiênico', }
 ]
 
 valor_promoção = 0.1 # %
