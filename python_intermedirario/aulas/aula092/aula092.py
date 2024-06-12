94c43b23f5af56b1bbbfcbaae39cf8d0269ef80c
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-06-12 12:24:08 -0300

    Update aula092.py

--- python_intermedirario/aulas/aula092/aula092.py
+++ python_intermedirario/aulas/aula092/aula092.py
@@ -8,6 +8,6 @@
 # O python conhece todos os módulos e pacotes presentes
 # nos caminhos de sys.path
 
-import modulo092
+import python_intermedirario.aulas.aula092.modulo092 as modulo092
 
 print('Este módulo se chama', __name__)
