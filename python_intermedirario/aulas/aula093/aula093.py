110a0acf320c2c555c41798506db5af83d5d076c
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-06-12 12:25:42 -0300

    update aula 093

---
+++ python_intermedirario/aulas/aula093/aula093.py
@@ -0,0 +1,8 @@
+
+import python_intermedirario.aulas.aula093.modulo093 as modulo093
+from python_intermedirario.aulas.aula093.modulo093 import soma, variavel_modulo
+
+print(modulo093.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(modulo093.soma(2, 3))
