bd3865225d08c32365673d4b608ce4cd46cdb3ec
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-06-19 17:45:43 -0300

    Update aula095.py

--- python_intermedirario/aulas/aula095.py
+++ python_intermedirario/aulas/aula095.py
@@ -1,12 +1,5 @@
-from aula095_package.modulo import soma_do_modulo, variavel, nova_variavel
+from aula095_package import soma_do_modulo, fala_oi
 
 
 print(soma_do_modulo(1, 2))
-
-print(variavel)
-print(nova_variavel)
-
-from aula095_package.modulo import fala_oi, soma_do_modulo
-
-print(__name__)
 fala_oi()
