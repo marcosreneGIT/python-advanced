c3f60b2d042bb2c54f4b357112aed36490a93702
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-06-26 15:35:25 -0300

    Update aula080.py

--- python_intermedirario/aulas/aula080.py
+++ python_intermedirario/aulas/aula080.py
@@ -1,27 +1,36 @@
-# Dictionary Comprehension e Set Comprehension
+# dictionary comprehension e set comprehension
+
 produto = {
-    'nome': 'Caneta Azul',
-    'preco': 2.5,
-    'categoria': 'Escritório',
+    'nome' : 'Caneta Azul',
+    'preco' : 2.5,
+    'cartegoria' : 'Escritório'
 }
 
-dc = {
-    chave: valor.upper()
-    if isinstance(valor, str) else valor
-    for chave, valor
-    in produto.items()
-    if chave != 'categoria'
+dicionario_0 = {
+    chave : valor.upper() if isinstance(valor, str)
+    else valor
+    for chave, valor in produto.items()
+    if chave != 'cartegoria'
 }
+
+print(dicionario_0, sep='\n')
 
 lista = [
     ('a', 'valor a'),
-    ('b', 'valor a'),
-    ('b', 'valor a'),
+    ('b', 'valor b'),
+    ('c', 'valor c'),
 ]
-dc = {
-    chave: valor
+
+dicionario_1 = {
+    chave : valor
     for chave, valor in lista
 }
 
-s1 = {2 ** i for i in range(10)}
-print(s1)
+print(dicionario_1, sep='\n')
+
+set_0 = {
+    2 ** i for i in range(10)
+    if 2 ** i < 100
+}
+
+print(set_0)
