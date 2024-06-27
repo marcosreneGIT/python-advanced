0d91f7a81ad39779c610e1523cae55e37d7aa763
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-06-27 10:07:22 -0300

    Update aula083.py

--- python_intermedirario/aulas/aula083.py
+++ python_intermedirario/aulas/aula083.py
@@ -1,9 +1,10 @@
 # dir, hasattr e getattr em Python
+
 string = 'Marcos'
-metodo = 'strip'
+metodo = 'lower'
 
 if hasattr(string, metodo):
-    print('Existe upper')
+    print(f'O método {metodo}() é plausível')
     print(getattr(string, metodo)())
 else:
     print('Não existe o método', metodo)
