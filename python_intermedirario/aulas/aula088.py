a30286aeb91fe89e9d9f62c93ff410fa5255a2c4
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-07-01 16:49:58 -0300

    Update aula088.py

--- python_intermedirario/aulas/aula088.py
+++ python_intermedirario/aulas/aula088.py
@@ -1,25 +1,30 @@
-# (Parte 2) try e except para tratar exceções
+# parte 2 - try e except para tratar exceções
 # a = 18
 # b = 0
-# c = a / b
+#
 
 try:
     a = 18
     b = 0
-    # print(b[0])
-    # print('Linha 1'[1000])
     c = a / b
-    print('Linha 2')
-except ZeroDivisionError as e:
-    print(e.__class__.__name__)
-    print(e)
-except NameError:
-    print('Nome b não está definido')
-except (TypeError, IndexError) as error:
-    print('TypeError + IndexError')
-    print('MSG:', error)
-    print('Nome:', error.__class__.__name__)
-except Exception:
-    print('ERRO DESCONHECIDO.')
 
-print('CONTINUAR')
+except ZeroDivisionError as e:
+    # print(e.__class__.__name__)
+    # print(e)
+    print('divisão por zero')
+
+except NameError:
+    print('variavel não definida')
+
+except(TypeError, IndexError) as e:
+    if e.__class__.__name__ == 'TypeError':
+        print('tipo invalido')
+
+    elif e.__class__.__name__ == 'IndexError':
+        print('indice invalido')
+        
+except Exception:
+    print('Um erro desconhecido ocorreu.')
+
+else:
+    print(f'O resultado da divisião é: {c}')
