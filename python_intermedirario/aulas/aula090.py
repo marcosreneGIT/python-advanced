d19084fd61707cb4008848314e985f4254b76ff2
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-07-08 14:22:55 -0300

    Update aula090.py

--- python_intermedirario/aulas/aula090.py
+++ python_intermedirario/aulas/aula090.py
@@ -1,26 +1,28 @@
 # raise - lançando exceções (erros)
 # https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
-def nao_aceito_zero(d):
+
+def zero_divisao_erro(d):
     if d == 0:
-        raise ZeroDivisionError('Você está tentando dividir por zero')
+        raise ZeroDivisionError ('Você está tentando dividir por zero (0).')
     return True
-
-
-def deve_ser_int_ou_float(n):
+    
+def obrigatoriedade_int_float (n):
     tipo_n = type(n)
-    if not isinstance(n, (float, int)):
+    
+    if not isinstance(n, (int, float)):
         raise TypeError(
-            f'"{n}" deve ser int ou float. '
+            f'"{n}" deve ser int ou float.'
             f'"{tipo_n.__name__}" enviado.'
         )
     return True
 
-
 def divide(n, d):
-    deve_ser_int_ou_float(n)
-    deve_ser_int_ou_float(d)
-    nao_aceito_zero(d)
+    obrigatoriedade_int_float(n)
+    obrigatoriedade_int_float(d)
+
+    zero_divisao_erro(d)
+
     return n / d
 
-
 print(divide(8, '0'))
+    
