637e6a17c1cada3dc5418ce0776ca6fbc90e26bd
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-07-08 14:39:01 -0300

    Update aula091.py

--- python_intermedirario/aulas/aula091.py
+++ python_intermedirario/aulas/aula091.py
@@ -1,42 +1,29 @@
-# Módulos padrão do Python (import, from, as e *)
+# módulos padrão do Python (import, from, as e *)
 # https://docs.python.org/3/py-modindex.html
+
+
 # inteiro - import nome_modulo
-# Vantagens: você tem o namespace do módulo
-# Desvantagens: nomes grandes
-# import sys
+# vantagens: você tem o namespace do módulo
+# desvantagens: nomes grandes
+# exemplo: import sys
 
-# platform = 'A MINHA'
-# print(sys.platform)
-# print(platform)
-
-# partes - from nome_modulo import objeto1, objeto2
-# Vantagens: nomes pequenos
-# Desvantagens: Sem o namespace do módulo
-# from sys import exit, platform
-
-# print(platform)
+# partes - from nome_modulo import objeto_0, objeto_1
+# vantagens: nomes pequenos
+# desvantagens: sem o namespace do módulo
+# exemplo: from sys import exit, platform
 
 # alias 1 - import nome_modulo as apelido
-# import sys as s
-
-# sys = 'alguma coisa'
-# print(s.platform)
-# print(sys)
-
+# exemplo: import sys as s
 
 # alias 2 - from nome_modulo import objeto as apelido
+# exemplos: 
 # from sys import exit as ex
 # from sys import platform as pf
 
-# print(pf)
-
-# Vantagens: você pode reservar nomes para seu código
-# Desvantagens: pode ficar fora do padrão da linguagem
+# vantagens: você pode reservar nomes para seu código
+# desvantagens: pode ficar fora do padrão da linguagem
 
 # má prática - from nome_modulo import *
-# Vantagens: importa tudo de um módulo
-# Desvantagens: importa tudo de um módulo
-# from sys import exit, platform
-
-# print(platform)
-# exit()
+# vantagens: importa tudo de um módulo
+# desvantagens: importa tudo de um módulo
+# exemplo: from sys import exit, platform
