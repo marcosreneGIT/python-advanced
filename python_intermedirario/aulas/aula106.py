5d19a71433f9b509513534097fe7d8b3e6c865b9
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-09-25 15:57:31 -0300

    Update aula106.py

--- python_intermedirario/aulas/aula106.py
+++ python_intermedirario/aulas/aula106.py
@@ -6,7 +6,27 @@ from functools import reduce
 numeros = [1, 2, 3, 4, 5]
 
 total_numeros = reduce(
-    lambda x, y: x * y, numeros, 0
+    lambda x, y: x * y, numeros, 10
 )
 
 print(total_numeros)
+
+
+produtos = [
+    {'nome': 'produto 1', 'preco': 10},
+    {'nome': 'produto 2', 'preco': 20},
+    {'nome': 'produto 3', 'preco': 30},
+    {'nome': 'produto 4', 'preco': 40},
+    {'nome': 'produto 5', 'preco': 50},
+]
+
+total_produtos = reduce(lambda acc, produto: acc + produto['preco'], produtos, 0)
+
+print(total_produtos)
+
+
+letras = ['B', 'C']
+
+total_letras = reduce(lambda acc, letra: acc + letra, letras, 'A')
+
+print(total_letras)
