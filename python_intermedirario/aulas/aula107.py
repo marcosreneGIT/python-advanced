49461ce949eaacfad39e3dac675bc15c435216b6
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-09-25 17:03:08 -0300

    Create aula107.py

---
+++ python_intermedirario/aulas/aula107.py
@@ -0,0 +1,19 @@
+# funções recursivas e recursividade
+
+# - funções que podem se chamar de volta
+# - úteis p/ dividir problemas grandes em partes menores
+
+# Toda função recursiva deve ter:
+# - Um problema que possa ser dividido em partes menores
+# - Um caso recursivo que resolve o pequeno problema
+# - Um caso base que para a recursão
+# - fatorial - n! = 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+def fatorial(n):
+    if n == 1:
+        return 1
+    else: 
+        return n * fatorial(n - 1)
+    
+    
+print(fatorial(5))
