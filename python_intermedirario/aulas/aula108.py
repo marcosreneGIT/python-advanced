a5d1892100f3b3f12931a89bed19bab2695ae873
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-09-26 15:50:32 -0300

    Create aula108.py

---
+++ python_intermedirario/aulas/aula108.py
@@ -0,0 +1,6 @@
+# ambientes virtuais em Python (venv)
+
+# um ambiente virtual carrega toda a sua instalação do Python para uma pasta no caminho escolhido.
+# ao ativar um ambiente virtual, a instalação do ambiente virtual será usada.
+# venv é o módulo que vamos usar para criar ambientes virtuais.
+# você pode dar o nome que preferir para um ambiente virtual, mas os mais comuns são: venv env .venv .env
