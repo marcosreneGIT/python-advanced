4f4b6f44a3e241f88b2151ba2ef821d2b2afc21e
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-11-12 23:55:32 -0300

    Create aula129.py

---
+++ python_poo/aulas/aula129.py
@@ -0,0 +1,6 @@
+# relações entre classes: associação, agregação e composição
+# agregação é uma forma mais especializada de associação entre dois ou mais objetos. 
+# Cada objeto terá seu ciclo de vida independente.
+# Geralmente é uma relação de um para muitos, onde um objeto tem um ou muitos objetos.
+# Os objetos podem viver separadamente, mas pode se tratar de uma relação onde um objeto precisa de outro para fazer determinada tarefa.
+# (existem controvérsias sobre as definições de agregação).
