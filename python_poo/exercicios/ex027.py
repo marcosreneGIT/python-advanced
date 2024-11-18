893d93a74b1f382d381fb65386a8d672b588ba98
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-11-18 16:57:18 -0300

    Create ex027.py

---
+++ python_poo/exercicios/ex027.py
@@ -0,0 +1,22 @@
+# exercício com classes
+
+# 1 - crie uma classe Carro, Motor e Fabricante
+
+# 2 - faça a ligação entre Carro tem um Motor
+# obs.: Um motor pode ser de vários carros
+
+# 3 - faça a ligação entre Carro e um Fabricante
+# obs.: Um fabricante pode fabricar vários carros
+
+# exiba o nome do carro, motor e fabricante na tela
+
+class Carro:
+    pass
+
+
+class Motor:
+    pass
+
+
+class Fabricante:
+    pass
