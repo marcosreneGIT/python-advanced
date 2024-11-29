0004cd7ebadd9b9ee228c18e3eb115d43b6b8208
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-11-29 20:18:01 -0300

    Update eletronico.py

--- python_poo/aulas/aula134.py/eletronico.py
+++ python_poo/aulas/aula134.py/eletronico.py
@@ -1,3 +1,6 @@
+from log import LogPrintMixin
+
+
 class Eletronico:
     def __init__(self, nome):
         self._nome = nome
@@ -10,3 +13,17 @@ class Eletronico:
     def delisgar(self):
         if self._ligado:
             self._ligado = False
+            
+
+class Smartphone(Eletronico, LogPrintMixin):
+    def ligar(self):
+        super().ligar()
+        
+        if self._ligado:
+            self.log_succes(f'O {self._nome} está ligado.')
+
+    def delisgar(self):
+        super().delisgar()
+
+        if not self._ligado:
+            self.log_error(f'Não foi possivel ligar o {self._nome}.')
