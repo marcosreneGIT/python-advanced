017da88ea0b16ff50b860a82919514b2f0e65f51
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-11-28 21:50:19 -0300

    Create eletronico.py

---
+++ python_poo/aulas/aula134.py/eletronico.py
@@ -0,0 +1,12 @@
+class Eletronico:
+    def __init__(self, nome):
+        self._nome = nome
+        self._ligado = False
+
+    def ligar(self):
+        if not self._ligado:
+            self._ligado = True
+
+    def delisgar(self):
+        if self._ligado:
+            self._ligado = False
