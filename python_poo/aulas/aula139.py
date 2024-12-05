07ff0b1e2b8ce50a53e6a06b932df7a26427ccfb
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-12-05 16:39:08 -0300

    Create aula139.py

---
+++ python_poo/aulas/aula139.py
@@ -0,0 +1,13 @@
+# teoria: Python Special Methods ou Dunder Methods
+
+# Dunder = Double Underscore = __dunder__
+# artigos: https://rszalski.github.io/magicmethods/ 
+         # https://docs.python.org/3/reference/datamodel.html#specialnames
+
+# exemplos: 
+# __lt__(self, other) : self < other 
+# __gt__(self, other) : self > other 
+
+# __str__(self) : str
+
+# existe inumeros metodos, por isso é recomendada a leitura da documentação
