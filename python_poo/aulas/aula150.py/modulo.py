8ce93050a19ea14cc95df36fb8949bf6060f2369
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-12-23 10:30:59 -0300

    Update modulo.py
    DocStrings de várias linhas (Documentação)

--- python_poo/aulas/aula150.py/modulo.py
+++ python_poo/aulas/aula150.py/modulo.py
@@ -1,4 +1,25 @@
-"""DocString do módulo"""
+"""DocString do módulo
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat 
+fermentum urna eget aliquam. Suspendisse vehicula cursus arcu et auctor. Morbi 
+ligula nulla, imperdiet imperdiet dapibus in, rhoncus ut leo. Quisque luctus, mi
+vel pretium posuere, augue metus hendrerit justo, at imperdiet nunc ligula ut 
+dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+cubilia curae; Pellentesque nec sem ligula. Suspendisse tellus ex, semper non 
+elit sit amet, sagittis feugiat nibh. Quisque accumsan eros nec diam ultrices, 
+placerat eleifend lorem pharetra. Fusce at ex eu sapien suscipit ultricies eget 
+ac turpis. Duis interdum suscipit consectetur. Vestibulum imperdiet egestas mi, 
+sit amet interdum mi. Integer viverra imperdiet neque, id eleifend nisl 
+vulputate et. Nunc nec hendrerit felis. Nam vel metus blandit, fermentum justo 
+vitae, tincidunt augue. Pellentesque habitant morbi tristique senectus et netus 
+et malesuada fames ac turpis egestas.
+
+Etiam sit amet commodo ante. Fusce in porta sem. Fusce mollis ex at sapien 
+placerat consequat. Cras vel arcu vitae diam auctor interdum vel sed metus. 
+Donec id accumsan nisi. Nullam sed maximus ligula. Quisque rhoncus lacinia 
+bibendum. Fusce et aliquam massa. Maecenas est sapien, consectetur id rutrum a, 
+scelerisque quis eros.
+"""
 
 var = 1
 
