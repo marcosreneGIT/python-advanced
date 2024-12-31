be0f3b8bab13ee3be2e12bc3cc9b8da2cd0280b2
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2024-12-31 15:33:34 -0300

    Update aula152.py

--- python_poo/aulas/aula152.py
+++ python_poo/aulas/aula152.py
@@ -1,26 +1,27 @@
 # membro = Classe(valor), Classe['chave']
-# chave = Classe.chave.name
-# valor = Classe.chave.value
+# chave  = Classe.chave.name
+# valor  = Classe.chave.value 
 
 import enum
 
 def mover(direcao):
     print(f'Movendo para {direcao}')
-# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
 
-
+    # Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'Direita'])
+    
 mover('esquerda')
 mover('direita')
 mover('acima')
 mover('abaixo')
+
 
 class Direcoes(enum.Enum):
     ESQUERDA = enum.auto()
     DIREITA = enum.auto()
     ACIMA = enum.auto()
     ABAIXO = enum.auto()
-    
-    
+
+
 print(Direcoes(1), Direcoes['ESQUERDA'], Direcoes.ESQUERDA)
 print(Direcoes(1).name, Direcoes.ESQUERDA.value)
 
@@ -30,8 +31,7 @@ def mover(direcao: Direcoes):
         raise ValueError('Direção não encontrada')
     print(f'Movendo para {direcao.name} ({direcao.value})')
     
-    
 mover(Direcoes.ESQUERDA)
 mover(Direcoes.DIREITA)
 mover(Direcoes.ACIMA)
-mover(Direcoes.ABAIXO)
+mover(Direcoes.ABAIXOs)
