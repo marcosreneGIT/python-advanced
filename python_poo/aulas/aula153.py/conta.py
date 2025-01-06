755e9c203c79a8fbf86c89700df9fa79ff8ce6e2
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-01-06 19:55:38 -0300

    Update conta.py
    class ContaCorrente

--- python_poo/aulas/aula153.py/conta.py
+++ python_poo/aulas/aula153.py/conta.py
@@ -9,16 +9,42 @@ class Conta(abc.ABC):
         self.saldo = saldo
         
     def detalhes(self, msg=''):
-        print(f'{msg}O seu saldo atual é de: R${self.saldo:.2f}.')
+        print(f'{msg}O seu saldo atual é de: R${self.saldo:.2f}.\n')
         
     def depositar(self, valor):
         self.saldo += valor
         self.detalhes(f'Deposito de R${valor:.2f} concluído.\n')
 
-    # @abc.abstractmethod
-    # def sacar(self, valor): ...
+    @abc.abstractmethod
+    def sacar(self, valor): ...
     
-pessoa = Conta(1, 10, 0)
 
-pessoa.depositar(50)
+class ContaCorrente(Conta):
+    limite_extra = 200
+    
+    def sacar(self, valor):
+        print(f'Valor que você deseja sacar R$:{valor:.2f}')
         
+        if valor <= (self.saldo + self.limite_extra):
+            print(f'Você sacou R${valor:.2f}'
+                  f' do seu saldo de R${self.saldo:.2f}')
+            
+            if valor >= self.saldo:
+                print('Você utilizou seu limite extra.\nSeu saldo atual é de '
+                      f'{(self.saldo + self.limite_extra) - valor:.2f}')
+                return
+            else:
+                print(f'Seu saldo atual é de R${self.saldo - valor:.2f}')
+                return
+                
+        return 'Você não possui saldo suficiente.'
+                
+
+# class ContaPoupanca(Conta):
+#     def sacar(self, valor):
+#         ...
+
+pessoa = ContaCorrente(1, 10, 500)
+
+pessoa.depositar(500)
+print(pessoa.sacar(1200))
