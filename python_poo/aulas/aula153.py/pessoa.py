c6abe53973bbe3a96215001e9c6291c622c6bdd3
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-01-20 10:55:01 -0300

    Update pessoa.py
    Criando a classe Cliente

--- python_poo/aulas/aula153.py/pessoa.py
+++ python_poo/aulas/aula153.py/pessoa.py
@@ -1,3 +1,5 @@
+import conta
+
 class Pessoa:
     def __init__(self, nome: str, idade: int) -> None:
         self._nome  = nome
@@ -19,4 +21,21 @@ class Pessoa:
     def idade(self, idade: int) -> None:
         self._idade = idade
         
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f'{class_name}: ({self.nome!r}, {self.idade!r})'
     
+    
+class Cliente(Pessoa):
+    def __init__(self, nome: str, idade: int) -> None:
+        super().__init__(nome, idade)
+        self.conta: conta.Conta | None = None
+        
+        
+if __name__ == '__main__':
+    cliente_0 = Cliente('Marcos', 21)
+    
+    cliente_0.conta = conta.ContaCorrente(1, 10, 100, 1000)
+    
+    print(cliente_0, '\n')
+    cliente_0.conta.sacar(100)
