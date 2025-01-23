270724df64727a921a945d4e1b3c3bb9e5e788db
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-01-23 16:55:19 -0300

    Update aula154.py
    __init__ e __post_init__ em dataclasses

--- python_poo/aulas/aula154.py
+++ python_poo/aulas/aula154.py
@@ -8,11 +8,18 @@
 
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(init=False) # possíbilidade de criar meu próprio __init__ 
 class Pessoa:
     _nome: str
     _idade: int
     
+    def __init__(self, nome, idade) -> None:
+        self._nome = nome
+        self._idade = idade
+    
+    # def __post_init__(self): # execulta após o "__init__" da dataclass.
+    #     print('__post_init__')
+        
     @property
     def nome(self):
         return self._nome
