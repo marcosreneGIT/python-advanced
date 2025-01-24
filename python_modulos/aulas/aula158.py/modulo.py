8c922b2891e649c39182061961fd2e82e871822b
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-01-24 08:26:42 -0300

    Creat aula158.py
     if __name__ == "__main__"

---
+++ python_modulos/aulas/aula158.py/modulo.py
@@ -0,0 +1,16 @@
+def soma(x: float, y: float) -> float:
+    
+    return x + y
+
+
+def main() -> None:
+    
+    print(soma(1, 2))
+    
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
