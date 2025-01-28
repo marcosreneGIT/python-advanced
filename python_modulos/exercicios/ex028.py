2d82a89e10db018ec85da0acf0e174709bec9979
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-01-28 19:23:12 -0300

    Update ex028.py

--- python_modulos/exercicios/ex028.py
+++ python_modulos/exercicios/ex028.py
@@ -1,7 +1,7 @@
 # Exercício solucionado: calculando as datas e parcelas de um empréstimo
 
 # Maria pegou um empréstimo de 1.000.000
-# para realizar o pagamento em 5 anos.
+# para realizar o pagamento em 5 anos e 6 meses.
 
 # A data em que ela pegou o empréstimo foi
 # 20/12/2020 e o vencimento de cada parcela
@@ -9,34 +9,29 @@
 
 # - Crie a data do empréstimo
 # - Crie a data do final do empréstimo
-# - Mostre todas as datas de vencimento e o valor de cada parcela
+# - Mostre todas as datas de vencimento apos 2 anos e o valor de cada parcela
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
-valor_emprestimo = 1_000_000
+valor = 100_000
 periodo_pagamento = relativedelta(years=5, months=6)
 
 data_inicio_emprestimo = datetime(2020, 12, 20)
-data_final_emprestimo = data_inicio_emprestimo + periodo_pagamento
+data_vencimento = data_inicio_emprestimo + relativedelta(years=2)
+data_final_emprestimo = data_vencimento + periodo_pagamento
 
-
-diferenca_data = relativedelta(data_final_emprestimo, data_inicio_emprestimo)
-quantidade_parcelas_emprestimo = diferenca_data.years * 12 + \
-                                 diferenca_data.months
-
-valor_parcela_emprestimo = valor_emprestimo / quantidade_parcelas_emprestimo
-
-data_vencimento_parecela = data_inicio_emprestimo + relativedelta(months=1)
-
-while data_vencimento_parecela <= data_final_emprestimo:
-    print(f'DATA DE VENCIMENTO: {data_vencimento_parecela.strftime('%d/%m/%Y')}'
-          f'\nVALOR DA PARCELA: R${valor_parcela_emprestimo:,.2f}\n')
-    
-    data_vencimento_parecela += relativedelta(months=1)    
-
-
-
-
-
+datas_parecelas: list[datetime] = []
+while data_vencimento < data_final_emprestimo:
+      datas_parecelas.append(data_vencimento)
+      
+      data_vencimento += relativedelta(months=1)
+      
+quantidade_parcelas = len(datas_parecelas)
+valor_parcela = valor / quantidade_parcelas
+      
+for i, data in enumerate(datas_parecelas):
+      print(f'NÚMERO DA PARECELA: {i+1}\n'
+            f'DATA DE VENCIMENTO: {data.strftime('%d/%m/%Y')}\n'
+            f'VALOR DA PARECELA: {valor_parcela:,.2f}\n')
