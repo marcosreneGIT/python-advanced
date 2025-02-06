15972f98781a0a94f8b6dfd2d23e34ab628957c9
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-02-06 19:01:52 -0300

    Update aula174.py

--- 04-modulos/aulas/aula174.py/aula174.py
+++ 04-modulos/aulas/aula174.py/aula174.py
@@ -1,13 +1,13 @@
-import json
-import os
+# json.dump e json.load com arquivos
+
+import os, json
+
 NOME_ARQUIVO = 'aula174.json'
-CAMINHO_ABSOLUTO_ARQUIVO = os.path.abspath(
-    os.path.join(
-        os.path.dirname(__file__),
-        NOME_ARQUIVO
+CAMINHO_ABSOLUTO = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), NOME_ARQUIVO)
     )
-)
-filme = {
+
+filmes = {
     'title': 'O Senhor dos Anéis: A Sociedade do Anel',
     'original_title': 'The Lord of the Rings: The Fellowship of the Ring',
     'is_movie': True,
@@ -16,8 +16,11 @@ filme = {
     'characters': ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Boromir'],
     'budget': None
 }
-with open(CAMINHO_ABSOLUTO_ARQUIVO, 'w') as arquivo:
-    json.dump(filme, arquivo, ensure_ascii=False, indent=2)
-with open(CAMINHO_ABSOLUTO_ARQUIVO, 'r') as arquivo:
-    filme_do_json = json.load(arquivo)
-    print(filme_do_json)
+
+with open(CAMINHO_ABSOLUTO, 'w') as arquivo:
+    json.dump(filmes, arquivo, ensure_ascii=False, indent=2)
+    
+with open(CAMINHO_ABSOLUTO, 'r') as arquivo:
+    filme = json.load(arquivo)
+    
+print(filme)
