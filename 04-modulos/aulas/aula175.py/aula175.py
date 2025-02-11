571d93db11b5f3b8e5dd85c526efe6f24e49c603
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-02-11 19:28:38 -0300

    Upadate aula175.py

--- 04-modulos/aulas/aula175.py/aula175.py
+++ 04-modulos/aulas/aula175.py/aula175.py
@@ -13,7 +13,7 @@ print(caminho.absolute()) # = caminho escrito
 print(caminho_arquivo.parent.parent) # = diretorio "pai"/"avô"...
 
 arquivo_json = caminho_arquivo.parent / 'aula175.json' 
-print(arquivo_json) # caminho com um arquivo jsonf
+print(arquivo_json) # caminho com um arquivo json
 
 arquivo_json.touch() # cria o arquivo 
 arquivo_json.unlink() # apaga o arquivo
@@ -21,3 +21,14 @@ arquivo_json.unlink() # apaga o arquivo
 arquivo_json.touch()
 arquivo_json.write_text('Olá, Mundo!') # escreve no arquivo
 print(arquivo_json.read_text()) # lê o que esta ecrito no arquivo
+
+
+arquivo_json.unlink()
+caminho_arquivo_json = Path(__file__).parent / 'aula175.json'
+
+with caminho_arquivo_json.open('a+') as arquivo: # escrevendo em multiplas linhas
+    arquivo.write('A\n')
+    arquivo.write('B\n')
+    arquivo.write('C\n')
+    
+print(caminho_arquivo_json.read_text())
