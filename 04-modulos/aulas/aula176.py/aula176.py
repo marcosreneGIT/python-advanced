abf0eb35d2d84c8c5a73960507e382c942337df7
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-02-12 08:40:30 -0300

    Update aula176.py
    Manipulando caminhos, pastas e arquivos no Python com pathlib

--- 04-modulos/aulas/aula176.py/aula176.py
+++ 04-modulos/aulas/aula176.py/aula176.py
@@ -9,3 +9,18 @@ caminho_pasta.mkdir(exist_ok=True) # criando um diretorio
 caminho_subpasta.mkdir(exist_ok=True) # criando outro diretorio #dentro do anterrior
 
 caminho_subpasta_arquivo.touch(exist_ok=True) # criando um arquivo dentro da subpasta
+
+def rmtree(root: Path, remove_root=True):
+    for file in root.glob('*'):
+        if file.is_dir():
+            print(f'Dir: {file}')
+            rmtree(file, False)
+            file.rmdir()
+        else:
+            print(f'File: {file}')
+            file.unlink
+        
+    if remove_root:
+        root.rmdir()
+            
+rmtree(caminho_pasta)
