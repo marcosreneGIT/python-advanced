ca2415828c33b79d4e4474c862ab227f2724e8a5
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-02-17 17:22:30 -0300

    Create aula177.py
    CSV (Comma Separated Values | Valores separados por vírgulas)

---
+++ 04-modulos/aulas/aula177.py/aula177.py
@@ -0,0 +1,27 @@
+# CSV (Comma Separeted Values | Valores Separados por Vírgula)
+
+# É um formato de arquivo que armazena dados em forma de tabela. 
+# Cada linha representa uma linda da tabela e as colunas são separadas por vírgulas.
+
+# É amplamente utilizado para tranferir dados entre sistemas de diferentes plataformas.
+# Exemplo:
+    # - Importar dados de uma planilha (Excel, etc.) para uma base de dados.
+    
+# Um arquivo CSV geralmente tem a extensão ".csv" e pode ser aberto em um editor
+# de texto ou em uma planilha eltrônica.
+
+# Exemplo de arquivo CSV:
+
+# Nome,Idade,Endereço # Nomes das colunas
+# Renê Santos,12,"Rua Um, 114, Centro"
+# Marcos Gomes,21,"Av Brasil, 21, Centro" # O uso de aspas duplas ("") para poder 
+                                            # ultizar a virgula (,) como informação
+
+# A primeira linha do arquivo define os nomes das colunas.
+# As linhas abaixo contêm os valores das linhas, separadas por vírgulas.
+
+# Regras simples do CSV:
+    # - Separe os valores das colunas por um delimitador único (,)
+    # - Cada registro deve estar em uma linha
+    # - Não deixar linhas ou espaços sobrando 
+    # - Use o caractere de escape (") quando o delimitador aparecer no valor.
