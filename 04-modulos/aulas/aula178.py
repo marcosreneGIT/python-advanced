1d20b44e347e9f327ea5f74f3fe2c302075fe084
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-02-19 11:23:53 -0300

    Update aula178.py

--- 04-modulos/aulas/aula178.py
+++ 04-modulos/aulas/aula178.py
@@ -1,18 +1,17 @@
 # csv.reader e csv.DictReader
-# csv.reader lê o CSV em formato de lista
-# csv.DictReader lê o CSV em formato de dicionário
+
+# csv.reader lê em formato de lista
+# csv.DictReader lê em formato de dicionário
 
 import csv
 from pathlib import Path
 
 
 CAMINHO_CSV = Path(__file__).parent / 'aula177.py' / 'aula177.csv'
+
 with open(CAMINHO_CSV, 'r') as arquivo:
-    leitor = csv.DictReader(arquivo)
-    for linha in leitor:
-        print(linha['Nome'], linha['Idade'], linha['Endereço'])
-        
-# with open(CAMINHO_CSV, 'r') as arquivo:
-#     leitor = csv.reader(arquivo)
-#     for linha in leitor:
-#         print(linha)
+    lista = csv.reader(arquivo) # csv.DictReader faria o mesmo 
+                                # so que em formato de dicionário
+    
+    for linha in lista:
+        print(linha)
